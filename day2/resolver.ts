f68162bfdc30c4e6b1150b0ca41ef99f1791fdef
Santiago M.A <santiagomartinagra@gmail.com>
2018-07-08 18:21:19 +0200

    feat(day2): 2nd solution

--- day2/resolver.ts
+++ day2/resolver.ts
@@ -1,8 +1,7 @@
 export const resolver = (numbers: number[]): number[] => {
-    return numbers.map((n, index) => {
-        const arrayWithoutN = numbers.filter((_, index2) => index !== index2);
-        return arrayWithoutN.reduce((n2, n3) => {
-            return n2 * n3;
-        });
+    return numbers.map((n) => {
+        return numbers.reduce((total, n2) => {
+            return n2 !== n ? total * n2 : total;
+        }, 1);
     });
 };
